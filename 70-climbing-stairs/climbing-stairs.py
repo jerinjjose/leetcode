0fe228d924602b857a64d855e4250c907b23b70f
Jerin Jose <jerinjose.j@gmail.com>
2024-02-22 12:28:42 +0530

    Time: 37 ms (46.11%) | Memory: 16.5 MB (94.59%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,9 +1,12 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        a, b, c = 0, 1, 0
         
-        for i in range(n):
-            c = a + b
-            a, b = b, c
-        
-        return b 
+        memo = {}
+        return self.helper(n, memo)
+    
+    def helper(self, n: int, memo: dict[int, int]) -> int:
+        if n == 0 or n == 1:
+            return 1
+        if n not in memo:
+            memo[n] = self.helper(n-1, memo) + self.helper(n-2, memo)
+        return memo[n]
