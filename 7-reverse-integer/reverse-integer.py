1d230d5b5bf5d99a2ea936a514a9aed7fef1e687
Jerin Jose <jerinjose.j@gmail.com>
2024-02-25 10:50:58 +0530

    Time: 32 ms (84.84%) | Memory: 16.4 MB (97.74%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.py
@@ -0,0 +1,20 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        upper_limit = (2 ** 31) -1
+        ouput = 0
+        is_negative = x < 0
+        if is_negative:
+            x = -x
+        while x > 0:
+            last_digit = x % 10
+            if ouput > (upper_limit - last_digit) // 10:
+                return 0
+            ouput = ouput * 10 + last_digit
+            x = x // 10
+        
+        if is_negative:
+            ouput = -ouput
+        return ouput
+
+
+
