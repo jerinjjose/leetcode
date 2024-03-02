1008d50f6d4d1f67cfb09882608fc8a4894df6a9
Jerin Jose <jerinjose.j@gmail.com>
2024-03-02 16:17:16 +0530

    Time: 32 ms (96.91%) | Memory: 16.7 MB (49.24%) - LeetSync

--- 38-count-and-say/count-and-say.py
+++ 38-count-and-say/count-and-say.py
@@ -1,23 +1,22 @@
 class Solution:
     def countAndSay(self, n: int) -> str:
-        if n == 1:
-            return "1"
-        
-        previous_sequence = self.countAndSay(n - 1)
-        result = ""
-        current_char = previous_sequence[0]
-        count = 1
-        
-        for i in range(1, len(previous_sequence)):
-            if previous_sequence[i] == current_char:
-                count += 1
+        countString = "1"
+        for _ in range(n - 1):
+            countString = self.countTheString(countString)
+        return countString
+
+    def countTheString(self, count: str) -> str:
+        formattedString = []
+        counter = 1
+        prev = count[0]
+        for index in range(1, len(count)):
+            if count[index] == prev:
+                counter += 1
             else:
-                result += str(count)
-                result += str(current_char)
-                current_char = previous_sequence[i]
-                count = 1
-        
-        result += str(count)
-        result += str(current_char)
-        
-        return result
+                formattedString.append(str(counter))
+                formattedString.append(prev)
+                counter = 1
+                prev = count[index]
+        formattedString.append(str(counter))
+        formattedString.append(prev)
+        return ''.join(formattedString)
