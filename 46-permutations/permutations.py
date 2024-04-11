caea07713df836fabb241b5da95b69710aca9546
Jerin Jose <jerinjose.j@gmail.com>
2024-04-11 22:24:15 +0530

    Time: 48 ms (12.15%) | Memory: 16.8 MB (37.03%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -1,13 +1,15 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         def backtrack(start):
+            # If we're at the end of the list, we have a complete permutation
             if start == len(nums):
                 permutations.append(nums[:])
             else:
+                # Swap the current element with each element that comes after it
                 for i in range(start, len(nums)):
-                    nums[start], nums[i] = nums[i], nums[start]
-                    backtrack(start + 1)
-                    nums[start], nums[i] = nums[i], nums[start]
+                    nums[start], nums[i] = nums[i], nums[start]  # swap
+                    backtrack(start + 1)  # generate permutations for the rest of the list
+                    nums[start], nums[i] = nums[i], nums[start]  # backtrack
 
         permutations = []
         backtrack(0)
